4a927942c47e85896028f659ddb2707f4d55c859
jessimaster <jessica@cashmans.org>
2017-10-30 23:22:37 -0400

    uses argv

--- Speech.py
+++ Speech.py
@@ -1,11 +1,18 @@
 # Speech.py
 # If no sound, in the cmd line type : "start-pulseaudio-x11"
 
+
 # Purpose: says the chosen Phrase
-import sys
+import sys, getopt
 import pyttsx
 
 def speak(phrase):
     Device = pyttsx.init()
     Device.say(phrase)
     Device.runAndWait()
+
+def main(argv):
+    speak(sys.argv[1])
+    return
+if __name__ == '__main__': #main(argv)
+    main(sys.argv[1:])
